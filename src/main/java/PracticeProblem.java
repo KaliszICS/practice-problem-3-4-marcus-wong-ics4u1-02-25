597185844bdd4e4db51bc272da6a343fe36adf7a
marcus-wong-ics4u1-02-25 <marcus.wong5@student.tdsb.on.ca>
2025-04-09 07:38:53 +0000

    same assignment 3.4

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,42 @@
+import java.util.*;
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
 
-	public static void q1() {
+	public static int[] recaman(int n) {
 		//Write question 1 code here
+		if (n <= 0) {
+			return new int[0];
+		}
+		int[] sequence = new int[n + 1];
+		HashSet<Integer> duplicate = new HashSet<>();
+		sequence[0] = 0;
+		duplicate.add(0);
+		recamanHelper(sequence, duplicate, 1);
+		int[] result = new int[n];
+		for (int i = 0; i < n; i++) {
+			result[i] = sequence[i + 1];
+		}
+		return result;
 	}
+	public static void recamanHelper(int[] sequence, HashSet<Integer> duplicate, int index) {
+		if (index >= sequence.length) {
+			return;
+		}
+		int last = sequence[index - 1];
+		int next = last - index;
+		if (next > 0 && !duplicate.contains(next)) {
+			sequence[index] = next;
+		} 
+		else {
+			sequence[index] = last + index; // subtract current step from teh last number, but when -ve then add it
+		}
 
-	public static void q2() {
-		//Write question 2 code here
+		duplicate.add(sequence[index]);
+
+		recamanHelper(sequence, duplicate, index + 1);
 	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
-	}
-
 }
